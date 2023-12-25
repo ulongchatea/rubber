5e3588b7dbdfa9028221fecede4cab019b98d3ac
ulongchatea <145919061+ulongchatea@users.noreply.github.com>
2023-12-26 00:29:50 +0900

    Update selenium_test.py

--- selenium/selenium_test.py
+++ selenium/selenium_test.py
@@ -12,7 +12,7 @@ naver_searchbox = driver.find_element(By.NAME, "query")
 naver_searchbox.send_keys("Automation")
 naver_searchbox.send_keys(Keys.RETURN)
 #driver.find_element(By.ID, "search-btn").click()
-
+#driver.find_element(By.XPATH, "//button[@class='btn btn-success']").click()
 
 
 time.sleep(10)
